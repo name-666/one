48b0192f1374f9d7ee728e2f6c7743939f85fdda
localhost\666 <999localhost@gmail.com>
2022-02-11 23:54:35 +0300

    Ответы

--- one/Program.cs
+++ one/Program.cs
@@ -6,7 +6,19 @@ namespace one
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Это моя первая программа?");
+            string answer1, answer2, question1, question2;
+
+            answer1 = " Программирование -  мир где нет границ, нет религии и цвета кожи. Лишь твоё воображения и разбитые надежды";
+            answer2 = "Хочу извратить своё мир как мир извратил моё мозг";
+
+            question1 = "Что для меня программирование?";
+            question2 = "Почему я хочу стать программистом ?";
+
+            Console.WriteLine( $"Первый вопрос {question1}    Ответ: {answer1}" +
+                $"Второй вопрос {question2}  Ответ: {answer2}");
+
+
+
         }
     }
 }
